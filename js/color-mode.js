de0679c8d2a12a85aaa16e497819ec448c2ec2b1
tsimeonov <tsimeonov12@gmail.com>
2025-07-25 13:10:42 +0200

    Update color-mode.js

--- js/color-mode.js
+++ js/color-mode.js
@@ -2,6 +2,12 @@
 
 const toggleButton = document.querySelector('#toggle-button');
 const root = document.querySelector(':root');
-
 const storageKey = 'color-mode';
 const defaultMode = 'light-mode';
+
+// Load the user's preffered color mode from local storage
+
+function loadColorMode() {
+	// Some code functionality here to run
+}
+loadColorMode();
