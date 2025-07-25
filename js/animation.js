4bcd24354206090cbdd967deb246c949feb8b0ac
tsimeonov <tsimeonov12@gmail.com>
2025-07-25 15:39:39 +0200

    Update animation.js

---
+++ js/animation.js
@@ -0,0 +1,21 @@
+//  Define DOM elements
+const heroImage = document.querySelector('#hero__animation__img');
+
+const tl = document.querySelector('#grid__tl');
+const tr = document.querySelector('#grid__tr');
+const bl = document.querySelector('#grid__bl');
+const br = document.querySelector('#grid__br');
+
+const tlBtn = document.querySelector('#grid__tl_btn');
+const trBtn = document.querySelector('#grid__tr_btn');
+const blBtn = document.querySelector('#grid__bl_btn');
+const brBtn = document.querySelector('#grid__br_btn');
+
+const tlContent = document.querySelector('#grid__tl_content');
+const trContent = document.querySelector('#grid__tr_content');
+const blContent = document.querySelector('#grid__bl_content');
+const brContent = document.querySelector('#grid__br_content');
+
+const projectOne = document.querySelector('.p-1');
+const projectTwo = document.querySelector('.p-2');
+const projectThree = document.querySelector('.p-3');
