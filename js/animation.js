b5a016b1ffd28f2b7fde39ad39d90ba9d341c8be
tsimeonov <tsimeonov12@gmail.com>
2025-07-30 20:06:39 +0200

    update

--- js/animation.js
+++ js/animation.js
@@ -1,20 +1,20 @@
-//  Define DOM elements
+// Define DOM elements
 const heroImage = document.querySelector('#hero__animation__img');
 
 const tl = document.querySelector('#grid__tl');
 const tr = document.querySelector('#grid__tr');
 const bl = document.querySelector('#grid__bl');
-// const br = document.querySelector('#grid__br');
+const br = document.querySelector('#grid__br');
 
 const tlBtn = document.querySelector('#grid__tl__btn');
 const trBtn = document.querySelector('#grid__tr__btn');
 const blBtn = document.querySelector('#grid__bl__btn');
-// const brBtn = document.querySelector('#grid__br__btn');
+const brBtn = document.querySelector('#grid__br__btn');
 
 const tlContent = document.querySelector('#grid__tl__content');
 const trContent = document.querySelector('#grid__tr__content');
 const blContent = document.querySelector('#grid__bl__content');
-// const brContent = document.querySelector('#grid__br__content');
+const brContent = document.querySelector('#grid__br__content');
 
 const projectOne = document.querySelector('.p-1');
 const projectTwo = document.querySelector('.p-2');
@@ -41,10 +41,10 @@ let brHidden = 'translateX(100vw) translateY(100vh)';
 // Define corner that is open
 let activeCorner = '';
 
-// Add an event listener to the window object to listen to resize events
+// Add an event listener to the window object to listen for resize events
 window.addEventListener('resize', handleWindowResize);
 
-// Function that handles the styling when resizing window
+// Function that hanldes the styling when resizing window
 function handleWindowResize() {
 	switch (activeCorner) {
 		case 'top-left':
@@ -57,21 +57,22 @@ function handleWindowResize() {
 				tlContent.style.display = 'flex';
 				tlContent.style.alignItems = 'center';
 				tlContent.style.justifyContent = 'center';
-				tlContent.style.background = 'var(--bg-tranparent)';
+				tlContent.style.background = 'var(--bg-transparent)';
 				tlContent.style.zIndex = '200';
 				tlBtn.style.zIndex = '300';
 				trBtn.style.zIndex = '100';
 				blBtn.style.zIndex = '100';
-				// brBtn.style.zIndex = '100';
+				brBtn.style.zIndex = '100';
 			} else {
 				tlActive = 'translateX(5vw) translateY(0)';
 				tlContent.style.transform = 'translateX(5vw) translateY(0)';
 				tlContent.style.width = '30vw';
 				tlContent.style.height = '0';
 				tlContent.style.top = '10vh';
-				tlContent.style.dispay = 'block';
+				tlContent.style.display = 'block';
 			}
 			break;
+
 		case 'top-right':
 			if (window.innerWidth <= 1100) {
 				trActive = 'translateX(0) translateY(0)';
@@ -82,19 +83,19 @@ function handleWindowResize() {
 				trContent.style.display = 'flex';
 				trContent.style.alignItems = 'center';
 				trContent.style.justifyContent = 'center';
-				trContent.style.background = 'var(--bg-tranparent)';
+				trContent.style.background = 'var(--bg-transparent)';
 				trContent.style.zIndex = '200';
 				trBtn.style.zIndex = '300';
 				tlBtn.style.zIndex = '100';
 				blBtn.style.zIndex = '100';
-				// brBtn.style.zIndex = '100';
+				brBtn.style.zIndex = '100';
 			} else {
 				trActive = 'translateX(-5vw) translateY(0)';
-				trContent.style.transform = 'translateX(5vw) translateY(0)';
+				trContent.style.transform = 'translateX(-5vw) translateY(0)';
 				trContent.style.width = '30vw';
 				trContent.style.height = '0';
 				trContent.style.top = '10vh';
-				trContent.style.dispay = 'block';
+				trContent.style.display = 'block';
 			}
 			break;
 
@@ -113,7 +114,7 @@ function handleWindowResize() {
 				trBtn.style.zIndex = '100';
 				tlBtn.style.zIndex = '100';
 				blBtn.style.zIndex = '300';
-				// brBtn.style.zIndex = '100';
+				brBtn.style.zIndex = '100';
 				projectOne.style.width = '70%';
 				projectOne.style.margin = 'auto auto 0.5rem';
 				projectTwo.style.width = '70%';
@@ -134,7 +135,7 @@ function handleWindowResize() {
 				trBtn.style.zIndex = '100';
 				tlBtn.style.zIndex = '100';
 				blBtn.style.zIndex = '300';
-				// brBtn.style.zIndex = '100';
+				brBtn.style.zIndex = '100';
 				projectOne.style.width = '40%';
 				projectOne.style.margin = 'auto auto 0.5rem';
 				projectTwo.style.width = '40%';
@@ -154,65 +155,33 @@ function handleWindowResize() {
 			}
 			break;
 
-			if (window.innerWidth <= 600) {
-				blActive = 'translateX(0) translateY(0)';
-				blContent.style.transform = 'translateX(0vw) translateY(0)';
-				blContent.style.width = '100vw';
-				blContent.style.height = '100vh';
-				blContent.style.top = '0';
-				blContent.style.display = 'flex';
-				blContent.style.alignItems = 'center';
-				blContent.style.justifyContent = 'center';
-				blContent.style.background = 'var(--bg-tranparent)';
-				blContent.style.zIndex = '200';
-				trBtn.style.zIndex = '100';
-				tlBtn.style.zIndex = '100';
-				blBtn.style.zIndex = '100';
-				// brBtn.style.zIndex = '100';
-				projectOne.style.margin = '70%';
-				projectOne.style.margin = 'auto auto 0.5rem';
-				projectTwo.style.margin = '70%';
-				projectTwo.style.margin = 'auto auto 0.5rem';
-				projectThree.style.margin = '70%';
-				projectThree.style.margin = 'auto auto 0.5rem';
-			} else if (window.innerWidth <= 1100) {
-				blActive = 'translateX(0) translateY(0)';
-				blContent.style.transform = 'translateX(0vw) translateY(0)';
-				blContent.style.width = '100vw';
-				blContent.style.height = '100vh';
-				blContent.style.top = '0';
-				blContent.style.display = 'flex';
-				blContent.style.alignItems = 'center';
-				blContent.style.justifyContent = 'center';
-				blContent.style.background = 'var(--bg-tranparent)';
-				blContent.style.zIndex = '200';
-				trBtn.style.zIndex = '100';
-				tlBtn.style.zIndex = '100';
-				blBtn.style.zIndex = '100';
-				// brBtn.style.zIndex = '100';
-				projectOne.style.margin = '40%';
-				projectOne.style.margin = 'auto auto 0.5rem';
-				projectTwo.style.margin = '40%';
-				projectTwo.style.margin = 'auto auto 0.5rem';
-				projectThree.style.margin = '40%';
-				projectThree.style.margin = 'auto auto 0.5rem';
-			} else {
-				blActive = 'translateX(10vw) translateY(7vh)';
-				blContent.style.transform = 'translateX(10vw) translateY(7vh)';
-				blContent.style.width = '15rem';
-				blContent.style.height = '0';
-				blContent.style.top = '40vh';
-				blContent.style.dispay = 'display';
-				projectOne.style.width = '100%';
-				projectTwo.style.width = '100%';
-				projectThree.style.width = '100%';
-			}
-
-			break;
-
 		case 'bottom-right':
-			// some code
+			if (window.innerWidth <= 1100) {
+				brActive = 'translateX(0) translateY(0)';
+				brContent.style.transform = 'translateX(0vw) translateY(0)';
+				brContent.style.width = '100vw';
+				brContent.style.height = '100vh';
+				brContent.style.bottom = '0';
+				brContent.style.display = 'flex';
+				brContent.style.flexDirection = 'column';
+				brContent.style.alignItems = 'center';
+				brContent.style.justifyContent = 'center';
+				brContent.style.background = 'var(--bg-transparent)';
+				brContent.style.zIndex = '200';
+				trBtn.style.zIndex = '100';
+				tlBtn.style.zIndex = '100';
+				blBtn.style.zIndex = '100';
+				brBtn.style.zIndex = '300';
+			} else {
+				brActive = 'translateX(-5vw) translateY(0)';
+				brContent.style.transform = 'translateX(-5vw) translateY(0)';
+				brContent.style.width = '30vw';
+				brContent.style.height = '0';
+				brContent.style.bottom = '30vh';
+				brContent.style.display = 'block';
+			}
 			break;
+
 		default:
 	}
 }
@@ -233,7 +202,6 @@ function playAnimation(animation, reverseAnimation) {
 			lastReverseAnimation = reverseAnimation;
 		}, 200);
 	} else {
-		// play forward code here
 		heroImage.classList.add(animation);
 		lastReverseAnimation = reverseAnimation;
 	}
@@ -243,7 +211,7 @@ function playClosingAnimation(reverseAnimation) {
 	tlBtn.innerHTML = 'About';
 	trBtn.innerHTML = 'Experience';
 	blBtn.innerHTML = 'Projects';
-	// brBtn.innerHTML = 'Contact';
+	brBtn.innerHTML = 'Contact';
 
 	switch (activeCorner) {
 		case 'top-left':
@@ -261,11 +229,12 @@ function playClosingAnimation(reverseAnimation) {
 			blBtn.style.color = textColor;
 			blContent.style.transform = blHidden;
 			break;
-		// case 'bottom-right':
-		// 	tlBtn.style.background = bgColor;
-		// 	tlBtn.style.color = textColor;
-		// 	tlContent.style.transform = tlHidden;
-		// 	break;
+		case 'bottom-right':
+			brBtn.style.background = bgColor;
+			brBtn.style.color = textColor;
+			brContent.style.transform = brHidden;
+			break;
+
 		default:
 	}
 
@@ -285,7 +254,7 @@ tlBtn.onclick = function () {
 	} else {
 		trBtn.innerHTML = 'Experience';
 		blBtn.innerHTML = 'Projects';
-		// brBtn.innerHTML = 'Contact';
+		brBtn.innerHTML = 'Contact';
 
 		// Setting activeCorner
 		activeCorner = 'top-left';
@@ -296,21 +265,19 @@ tlBtn.onclick = function () {
 
 		// Change background colors
 		trBtn.style.background = bgColor;
-		// brBtn.style.background = bgColor;
+		brBtn.style.background = bgColor;
 		blBtn.style.background = bgColor;
 		tlBtn.style.background = bgColorAlt;
 
 		// Change text colors
-
 		trBtn.style.color = textColor;
-		// brBtn.style.color = textColor;
+		brBtn.style.color = textColor;
 		blBtn.style.color = textColor;
 		tlBtn.style.color = textColorAlt;
 
 		// Change positions of the corner content
-
 		trContent.style.transform = trHidden;
-		// brContent.style.transform = brHidden;
+		brContent.style.transform = brHidden;
 		blContent.style.transform = blHidden;
 		tlContent.style.transform = tlActive;
 	}
@@ -322,7 +289,7 @@ trBtn.onclick = function () {
 	} else {
 		tlBtn.innerHTML = 'About';
 		blBtn.innerHTML = 'Projects';
-		// brBtn.innerHTML = 'Contact';
+		brBtn.innerHTML = 'Contact';
 
 		// Setting activeCorner
 		activeCorner = 'top-right';
@@ -333,21 +300,19 @@ trBtn.onclick = function () {
 
 		// Change background colors
 		trBtn.style.background = bgColorAlt;
-		// brBtn.style.background = bgColor;
+		brBtn.style.background = bgColor;
 		blBtn.style.background = bgColor;
 		tlBtn.style.background = bgColor;
 
 		// Change text colors
-
 		trBtn.style.color = textColorAlt;
-		// brBtn.style.color = textColor;
+		brBtn.style.color = textColor;
 		blBtn.style.color = textColor;
 		tlBtn.style.color = textColor;
 
 		// Change positions of the corner content
-
 		trContent.style.transform = trActive;
-		// brContent.style.transform = brHidden;
+		brContent.style.transform = brHidden;
 		blContent.style.transform = blHidden;
 		tlContent.style.transform = tlHidden;
 	}
@@ -358,71 +323,67 @@ blBtn.onclick = function () {
 		playClosingAnimation('reverse-animate-bottom-left');
 	} else {
 		tlBtn.innerHTML = 'About';
-		blBtn.innerHTML = 'Experience';
-		// brBtn.innerHTML = 'Contact';
+		trBtn.innerHTML = 'Experience';
+		brBtn.innerHTML = 'Contact';
 
 		// Setting activeCorner
 		activeCorner = 'bottom-left';
-		blBtn.innerHTML = 'Projects<br/>&darr';
+		blBtn.innerHTML = 'Projects<br/>&darr;';
 
 		handleWindowResize();
 		playAnimation('animate-bottom-left', 'reverse-animate-bottom-left');
 
 		// Change background colors
 		trBtn.style.background = bgColor;
-		// brBtn.style.background = bgColor;
+		brBtn.style.background = bgColor;
 		blBtn.style.background = bgColorAlt;
 		tlBtn.style.background = bgColor;
 
 		// Change text colors
-
 		trBtn.style.color = textColor;
-		// brBtn.style.color = textColor;
+		brBtn.style.color = textColor;
 		blBtn.style.color = textColorAlt;
 		tlBtn.style.color = textColor;
 
 		// Change positions of the corner content
-
-		trBtn.style.transform = trHidden;
-		// brBtn.style.transform = brHidden;
-		blBtn.style.transform = blActive;
-		tlBtn.style.transform = tlHidden;
+		trContent.style.transform = trHidden;
+		brContent.style.transform = brHidden;
+		blContent.style.transform = blActive;
+		tlContent.style.transform = tlHidden;
 	}
 };
 
-// brBtn.onclick = function () {
-// 	if (activeCorner === 'bottom-right') {
-// 		playClosingAnimation('reverse-animate-bottom-right');
-// 	} else {
-// 		tlBtn.innerHTML = 'About';
-// 		blBtn.innerHTML = 'Experience';
-// 		blBtn.innerHTML = 'Projects';
+brBtn.onclick = function () {
+	if (activeCorner === 'bottom-right') {
+		playClosingAnimation('reverse-animate-bottom-right');
+	} else {
+		tlBtn.innerHTML = 'About';
+		trBtn.innerHTML = 'Experience';
+		blBtn.innerHTML = 'Projects';
 
-// 		// Setting activeCorner
-// 		activeCorner = 'bottom-right';
-// 		brBtn.innerHTML = 'Contact<br/>&darr';
+		// Setting activeCorner
+		activeCorner = 'bottom-right';
+		brBtn.innerHTML = 'Contact<br/>&darr;';
 
-// 		handleWindowResize();
-// 		playAnimation('animate-bottom-right', 'reverse-animate-bottom-right');
+		handleWindowResize();
+		playAnimation('animate-bottom-right', 'reverse-animate-bottom-right');
 
-// 		// Change background colors
-// 		trBtn.style.background = bgColor;
-// 		brBtn.style.background = bgColorAlt;
-// 		blBtn.style.background = bgColor;
-// 		tlBtn.style.background = bgColor;
+		// Change background colors
+		trBtn.style.background = bgColor;
+		brBtn.style.background = bgColorAlt;
+		blBtn.style.background = bgColor;
+		tlBtn.style.background = bgColor;
 
-// 		// Change text colors
+		// Change text colors
+		trBtn.style.color = textColor;
+		brBtn.style.color = textColorAlt;
+		blBtn.style.color = textColor;
+		tlBtn.style.color = textColor;
 
-// 		trBtn.style.color = textColor;
-// 		brBtn.style.color = textColorAlt;
-// 		blBtn.style.color = textColor;
-// 		tlBtn.style.color = textColor;
-
-// 		// Change positions of the corner content
-
-// 		trBtn.style.transform = trHidden;
-// 		brBtn.style.transform = brActive;
-// 		blBtn.style.transform = blHidden;
-// 		tlBtn.style.transform = tlHidden;
-// 	}
-// };
+		// Change positions of the corner content
+		trContent.style.transform = trHidden;
+		brContent.style.transform = brActive;
+		blContent.style.transform = blHidden;
+		tlContent.style.transform = tlHidden;
+	}
+};
